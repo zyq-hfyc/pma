11eb0937a0b2fd01320428780e0ceb2a84491258
Mengsaner <793308981@qq.com>
2019-12-04 08:53:29 +0800

    增加log4j的依赖和配置

---
+++ src/main/java/com/fengchao/pma/controller/TestLog4j.java
@@ -0,0 +1,10 @@
+package com.fengchao.pma.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class TestLog4j {
+
+    Logger log = LoggerFactory.getLogger(Test.class);
+    public static void main(String[] args) {
+
+    }
+}
